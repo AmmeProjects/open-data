d3cef1f64197c77fc011752ef858063918760d6b
Miguel Simão <masimao@sapo.pt>
2026-08-12 08:08:40 +0200

    Enrich CPO metadata

--- src/api/entities/cpo.py
+++ src/api/entities/cpo.py
@@ -4,9 +4,13 @@ from dataclasses import dataclass
 @dataclass
 class CPO:
     id: str
+    id_ext: str
+    # ID Derived from External EVSE-ID (different from national operator ID)
     name: str
     country_code: str
     website: str
     vat_id: str
     telephone: str
-    id_parent: str = None  # For future use if we want to link CPOs to a parent organization
+    display_name: str = None
+    parent_id: str = None
+    # For future use if we want to link CPOs to a parent organization
---
+++ src/conversion/datex2_namespaces.py
@@ -0,0 +1,10 @@
+NS = {
+    "ns": "http://datex2.eu/schema/3/common",
+    "ns2": "http://datex2.eu/schema/3/locationExtension",
+    "ns3": "http://datex2.eu/schema/3/locationReferencing",
+    "ns4": "http://datex2.eu/schema/3/facilities",
+    "ns5": "http://datex2.eu/schema/3/commonExtension",
+    "ns6": "http://datex2.eu/schema/3/energyInfrastructure",
+    "ns7": "http://datex2.eu/schema/3/d2Payload",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+}
